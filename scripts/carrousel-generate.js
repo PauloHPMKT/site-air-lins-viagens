888cc99fa77c8295a88267c51ac5d092f1155b44
Paulo Sergio <pauloserg861@gmail.com>
2022-02-23 19:07:25 -0300

    adicionando estrutura do site

---
+++ scripts/carrousel-generate.js
@@ -0,0 +1,63 @@
+const destinationList = [
+    {
+        destination: '../assents/lisbon.png',
+        text: 'Lisboa - Portugal',
+    },
+    {
+        destination: '../assents/faro-algarve.png',
+        text: 'Faro - Portugal',
+    },
+    {
+        destination: '../assents/porto-portugal.png',
+        text: 'Porto - Portugal',
+    },
+    {
+        destination: '../assents/recife.png',
+        text: 'Recife - Brasil',
+    },
+    {
+        destination: '../assents/fortaleza.jpg',
+        text: 'Fortaleza - Brasil',
+    },
+    {
+        destination: '../assents/rio-de-janeiro.jpg',
+        text: 'Rio de Janeiro - Brasil',
+    },
+    
+]
+
+//id identification for generate photo-carrousel only
+const carrouselList = document.querySelector('#img')
+
+destinationList.map(item => {
+    carrouselList.innerHTML += `
+    <div class="box-destin">
+        <img src="${item.destination}">
+        <div class="info-travel">
+            <h3>${item.text}</h3>
+        </div>
+    </div>
+    `
+})
+
+
+//selectors to carrousel effect
+const imgs = document.querySelector('.container-carrousel')
+const img = document.querySelectorAll('.box-destin')
+
+console.log(img)
+
+//function to moviment carrousel
+let idx = 0
+
+function carrousel () {
+    idx++
+
+    if(idx > img.length - 1) {
+        idx = 0
+    }
+
+    imgs.style.transform = `translateX(${-idx * 165}px)`
+}
+
+setInterval(carrousel, 2100);
---
+++ scripts/year.js
@@ -0,0 +1,5 @@
+const year = document.querySelector('#year')
+
+const currentYear = new Date()
+
+year.innerHTML = currentYear.getFullYear()
